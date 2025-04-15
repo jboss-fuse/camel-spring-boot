d999173c788a112a3b5e2df32fa54fd4835405c9
Tom Cunningham <tcunning@redhat.com>
2025-04-15 15:01:38 -0400

    Close stream and fileReader before resource leak

--- tooling/camel-spring-boot-generator-maven-plugin/src/main/java/org/apache/camel/springboot/maven/RequiredProductizedArtifactsReader.java
+++ tooling/camel-spring-boot-generator-maven-plugin/src/main/java/org/apache/camel/springboot/maven/RequiredProductizedArtifactsReader.java
@@ -34,10 +34,8 @@ import java.util.HashMap;
     public static HashMap<String, Boolean> getProductizedCSBArtifacts(File requiredFile) {
         HashMap<String, Boolean> map = new HashMap<>();
 
-        try {
-            FileReader fileReader = new FileReader(requiredFile);
-            BufferedReader br = new BufferedReader(fileReader);
-
+        try (FileReader fileReader = new FileReader(requiredFile);
+        BufferedReader br = new BufferedReader(fileReader)) {
             String line;
             while ((line = br.readLine()) != null) {
                 if (line.startsWith("#")) {
